a251522be357f743cbe75cb7ed72b67de908fb15
Sid <sidharth.ramachandran@gmail.com>
2020-07-13 22:52:56 +0200

    Correction

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,7 @@
 set -e
 
 # Extract the base64 encoded config data and write this to the KUBECONFIG
-if [ "$UNCODED" ]
-then
+if [ $UNCODED == "false" ]; then
     export KUBECONFIG = "$KUBE_CONFIG_DATA"
 else
     echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
