10f4315628a72c43625fdc6abdbc529371040e47
Sid <sidharth.ramachandran@gmail.com>
2020-07-13 19:23:26 +0200

    Changes to not try to decode the file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-# Extract the base64 encoded config data and write this to the KUBECONFIG
-echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/config
+# Do not try to decode the KUBE_CONFIG_DATA and write this to the KUBECONFIG
+echo "$KUBE_CONFIG_DATA" > /tmp/config
 export KUBECONFIG=/tmp/config
 
 sh -c "kubectl${KUBECTL_VERSION:+.${KUBECTL_VERSION}} $*"
