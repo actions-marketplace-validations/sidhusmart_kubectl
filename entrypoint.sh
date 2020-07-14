3823ef61671245d5c7c1a1230c8c8d4df08af1f8
Sid <sidharth.ramachandran@gmail.com>
2020-07-14 20:44:09 +0200

    Correction - variable substitution

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 set -e
 
 # Provide the KUBECONFIG directly from the argument
-export KUBECONFIG = "$KUBE_CONFIG_DATA"
+export KUBECONFIG = ${KUBE_CONFIG_DATA}
 
 sh -c "kubectl${KUBECTL_VERSION:+.${KUBECTL_VERSION}} $*"
